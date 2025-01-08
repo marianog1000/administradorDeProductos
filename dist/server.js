28b33d8469ec711b1209ed104c1cf43746dd2340
Mariano Gauna <Mariano.Gauna@endava.com>
2025-01-08 18:38:27 -0300

    CLASE 352 – tsconfig para express y  Note.js

---
+++ dist/server.js
@@ -0,0 +1,7 @@
+"use strict";
+Object.defineProperty(exports, "__esModule", { value: true });
+exports.sumar = sumar;
+function sumar() {
+    console.log(6 + 6);
+}
+//# sourceMappingURL=server.js.map
